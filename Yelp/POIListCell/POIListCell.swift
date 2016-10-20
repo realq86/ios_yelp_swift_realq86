d5518ee3cb0e3abc8a84e24665e4a56b9dd50d20
Michael <realq86@gmail.com>
2016-10-20 13:41:57 -0700

    Moving the cell property setting to the cell class which takes the whole Business model.

--- Yelp/POIListCell/POIListCell.swift
+++ Yelp/POIListCell/POIListCell.swift
@@ -11,7 +11,31 @@ import UIKit
 class POIListCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var catagoryLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var starRatingImageView: UIImageView!
+    @IBOutlet weak var ratingsCountLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
 
+    var business:Business? {
+        didSet {
+            self.titleLabel.text = self.business?.name
+            if let imageURL = self.business?.imageURL {
+                self.backgroundImageView.setImageWith(imageURL, placeholderImage: nil)
+                print(imageURL)
+            }
+            
+            if let ratingImageURL = self.business?.ratingImageURL {
+                self.starRatingImageView.setImageWith(ratingImageURL)
+            }
+            
+            self.catagoryLabel.text = self.business?.categories
+            self.addressLabel.text = self.business?.address
+            self.ratingsCountLabel.text = (self.business?.reviewCount?.stringValue)! + " reviews"
+            self.distanceLabel.text = self.business?.distance
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
