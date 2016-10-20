621bf570b86c8b82fd2ac62528d8c18c826e6fba
Michael <realq86@gmail.com>
2016-10-20 13:42:16 -0700

    Set rounded rec of backgroundImageView.

--- Yelp/POIListCell/POIListCell.swift
+++ Yelp/POIListCell/POIListCell.swift
@@ -39,6 +39,7 @@ class POIListCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.backgroundImageView.layer.cornerRadius = 5
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
