0bb105cf511500bef14cd28de89b3c0a1fc4c106
Michael <realq86@gmail.com>
2016-10-22 13:43:46 -0700

    Fix auto layout in tableview cell.

---
+++ Yelp/FilterCheckCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilterCheckCell.swift
+//  Yelp
+//
+//  Created by Developer on 10/22/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class FilterCheckCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
