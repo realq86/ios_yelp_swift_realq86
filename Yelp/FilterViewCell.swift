42c17b7bc13f3bd7cc336dc17695f7e3fd7db133
Michael <realq86@gmail.com>
2016-10-21 21:45:50 -0700

    Setup Switch cell's event handling by manual target action.
    Respond by calling the delegate FilterViewController userDidChange(tableView:for:toValue:)

--- Yelp/FilterViewCell.swift
+++ Yelp/FilterViewCell.swift
@@ -17,6 +17,18 @@ class FilterViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.filterSwitch .addTarget(self, action: #selector(switchValueChanged(sender:)), for: .valueChanged)
+        
+    }
+    
+    func switchValueChanged(sender:UISwitch) {
+        print("Switch \(filterLabel.text) Changed to \(sender.isOn)")
+    //        self.delegate?.userDidChangeFilter!(cell: self, toValue: self.filterSwitch.isOn)
+        
+        if let currentTableView = self.getTableViewOf(view: self) {
+            self.delegate?.userDidChange!(tableView: currentTableView, for: self, toValue: self.filterSwitch.isOn)
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
